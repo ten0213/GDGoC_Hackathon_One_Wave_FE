5e7b33540a2c67849017b0d659c4eedae7a52ef5
황재현 <ten02130735@gmail.com>
2026-02-07 03:24:43 +0900

    vite.config.ts 수정

--- connectable/vite.config.ts
+++ connectable/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <- 여기 중요! 상대 경로로 바꿔야 Vercel에서 JS/CSS 404 안 뜸
 })
