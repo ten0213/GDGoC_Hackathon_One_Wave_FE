bab7bb8e2842a71bfa3bb58940fc27c8bc76d8fb
김한성 <hsung0714@gmail.com>
2026-02-06 20:21:29 +0900

    Feat: 루트 처리랑 에러 관련 문제 해결
    - 뭔 상위 페이지에서도 리엑트가 돌아가고 있다고 해서 그거 지움

--- connectable/src/App.tsx
+++ connectable/src/App.tsx
@@ -1,8 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage/HomePage'
 
 function App() {
-  return <HomePage />
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
